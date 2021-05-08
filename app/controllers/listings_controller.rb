4d9477844909b0a0c372e6d7f7c6504006ad83ef
Charlotte Weager <senwyn@googlemail.com>
2021-05-08 15:59:40 +0100

    Added user view, user controller, set up config

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,31 @@
+class ListingsController < ApplicationController
+  before_action :find_listing, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_listing
+    @listing = Listing.find(:id)
+  end
+end
