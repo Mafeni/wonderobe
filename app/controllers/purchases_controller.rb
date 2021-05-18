868f72a50336382a29dc6099613206d5bd452406
Charlotte Weager <senwyn@googlemail.com>
2021-05-18 21:45:52 +0100

    updated favourites

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,12 @@
 class PurchasesController < ApplicationController
-  before_action :find_listing
-  
+  before_action :find_listing, :find_favourites
+
   def new
   end
 
   def create
-    @purchase = Purchase.new()
+    @purchase = Purchase.new
+    @favourites.destroy_all
     @purchase.user = current_user
     @purchase.listing = @listing
     if @purchase.save
@@ -19,5 +20,9 @@ class PurchasesController < ApplicationController
 
   def find_listing
     @listing = Listing.find(params[:listing_id])
+  end
+
+  def find_favourites
+    @favourites = Favourite.where(listing: @listing)
   end
 end
