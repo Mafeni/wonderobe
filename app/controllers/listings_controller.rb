e99e153b09124575df0bc0183af6ebefeb7f4057
ceweager <66081334+ceweager@users.noreply.github.com>
2021-05-18 19:53:11 +0100

    Revert "Update user on page"

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,16 +12,15 @@ class ListingsController < ApplicationController
   end
 
   def show
-    @listing.punch(request)
     @top_listings = Listing.most_hit(1.month.ago, 9)
-    @reviews = Review.all.select { |review| review.purchase.listing.user == @listing.user }
+    @reviews = Review.all.select { |review| review.purchase.listing.user == @user }
     @transaction = Purchase.new
     if @reviews.present?
       @sum_of_ratings = 0
       @reviews.each do |review|
         @sum_of_ratings += review.rating.to_i
       end
-      @average_of_ratings = @sum_of_ratings / @reviews.count
+      @average_of_ratings = @sum_of_ratings / @listing.user.reviews.count
       @rounded_average_of_ratings = @average_of_ratings.round
     end
   end
