9e699debabf1ac9458dbd9823c63b8a4848bd487
Jose Blanco <jdblancoes@gmail.com>
2021-05-19 12:28:00 +0100

    Added gallery in the show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,13 +35,8 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import places from 'places.js';
-
-const initAutocomplete = () => {
-  const addressInput = document.getElementById('user_address');
-  if (addressInput) {
-    places({ container: addressInput });
-  }
-};
+import { initAutocomplete } from '../plugins/init_autocomplete';
+import { changePic } from '../plugins/gallery'
 
 
 document.addEventListener('turbolinks:load', () => {
@@ -49,4 +44,5 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   // $('.scroller').slick()
   initAutocomplete();
+  changePic();
 });
