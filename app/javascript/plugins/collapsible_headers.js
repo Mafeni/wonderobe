cc4ab6949092600167e4bdae410307a3abe9e213
Charlotte Weager <senwyn@googlemail.com>
2021-05-20 21:17:28 +0100

    added in switchable icons

--- app/javascript/plugins/collapsible_headers.js
+++ app/javascript/plugins/collapsible_headers.js
@@ -1,7 +1,14 @@
 
 const swapClassesOnClick = (event) => {
+  let button = event.currentTarget;
   let header = event.currentTarget.nextElementSibling;
   header.classList.toggle("active-card");
+  console.log(header.classList.contains("active-card"));
+  if (header.classList.contains("active-card")){
+    button.innerHTML = '<i class="fas fa-eye"></i>';
+  } else {
+    button.innerHTML = '<i class="fas fa-eye-slash"></i>';
+  };
 };
 
 const collapseHeaders = () => {
